e4d0c81d72d6b032a84ff8aa26d1f1fee3f635fb
Ercan <kadri503394@gmail.com>
2023-04-22 22:05:12 +0300

    feat: added CountrySelect, RentModal, useCountries and EcaniMap are created for Country and Maps

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import {FieldValues, useForm } from 'react-hook-form'
+import CountrySelect from '../inputs/CountrySelect'
+import dynamic from 'next/dynamic'
 
 
 enum STEPS  {
@@ -48,6 +50,11 @@ const RentModal = () => {
   })
 
   const category = watch('category')
+  const location = watch('location')
+
+  const EcaniMap = useMemo(() => dynamic(() => import('../EcaniMap'),{
+    ssr: false,
+  }), [location])
 
   const setCustomValue = (id: string, value: any) => { 
     setValue(id, value,{
@@ -103,11 +110,31 @@ const RentModal = () => {
       </div>
     </div>
   ) 
+
+
+  if(step === STEPS.LOCATİON){
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+        title='Where is your place located?'
+        subTitle='Help guests find your!'
+        />
+        <CountrySelect
+        value={location}
+        onChange={(value) => setCustomValue('location', value)}
+        />
+        <EcaniMap
+        center={location?.latlng}
+        />
+      </div>
+    )
+    }
+
   return (
     <Modal
     isOpen={rentModal.isOpen}
     onClose={rentModal.onClose}
-    onSubmit={rentModal.onClose}
+    onSubmit={onNext}
     actionLabel={actionLabel}
     secondaryActionLabel={secondaryActionLabel}
     secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
